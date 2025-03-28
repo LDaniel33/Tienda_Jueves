3de6557df07f10f8b3bdb93e60384522f72291b6
LDaniel33 <ldaniel33.m@gmail.com>
2025-03-27 19:32:14 -0600

    semana6

---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,21 @@
+
+package com.tienda.service;
+
+import com.tienda.domain.Categoria; 
+import java.util.List;
+
+public interface CategoriaService {
+
+    //se obtiene listado de la BD
+    public List<Categoria> getCategorias(boolean activos);// metodo de lista
+    
+    // Se obtiene un Categoria, a partir del id de un categoria
+    public Categoria getCategoria(Categoria categoria);
+    
+    // Se inserta un nuevo categoria si el id del categoria esta vacío
+    // Se actualiza un categoria si el id del categoria NO esta vacío
+    public void save(Categoria categoria);// guaradar la categoria
+    
+    // Se elimina el categoria que tiene el id pasado por parámetro
+    public void delete(Categoria categoria);// eliminar la categoria
+}
